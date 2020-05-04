37f1088acd79879e9b218a59a1632f2b0f254a2b
unknown <macjay314@gmail.com>
2020-05-04 12:33:34 -0700

    Create message to display to user before they enter their answer

--- RiddlesSphinx.cs
+++ RiddlesSphinx.cs
@@ -10,6 +10,12 @@ class RiddleSphinx
 {
   static void Main()
   {
-
+    Console.WriteLine("You stumble accross a great forboding sphinx. It's stare although lifeless, seems to follow you. All of a sudden, you hear a voice.");
+    string[] riddles = { "What's red and smells like blue paint?" };
+    Console.WriteLine("|| " + riddles[0] + " ||");
+    Console.WriteLine("The question echoes in your head. You quickly check your surroundings for anyone that may be lurking in the dark.");
+    Console.WriteLine("You find no one. As you remember what was transcribed to you, you notice that the words were clear in your head. That there was no clear point of origin.");
+    Console.WriteLine("Stricken by the silence in the room, you call out your answer:");
+    Console.WriteLine("Question: " + riddles[0]);
   }
 }
